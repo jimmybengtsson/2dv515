2b6f5a59c09cb99de1bb0700344b685b45b222bd
jimmybengtsson <jb223pu@student.lnu.se>
2018-11-09 11:20:44 +0100

    Assignment 1: Client React app init.

--- Assignment_1/server/app.js
+++ Assignment_1/server/app.js
@@ -5,7 +5,7 @@ let cors = require('cors');
 let bodyParser = require('body-parser');
 
 let app = express();
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 
